2d3e3a7b1b367a312e9ee915dd8fcc5d8596479d
anwar095 <54428215+anwar095@users.noreply.github.com>
2022-09-27 18:58:36 +0530

    Update buildAction.groovy

--- vars/buildAction.groovy
+++ vars/buildAction.groovy
@@ -4,7 +4,20 @@ def mavenBuild(final def args){
   if("$args.mavencommand" != 'default'){
     env.MAVENCOMMAND = "$args.mavencommand"
   } 
-  
+         sh '''
         cd $BUILDFOLDER
         $MAVENCOMMAND
+         sh '''
+}
+
+
+def buildPushImage(final def args){
+  env.DOCKERSCRIPTPATH = "build-docker-image.bash"
+    if("$args.imagebuildscript" != 'default'){
+      env.DOCKERSCRIPTPATH = "$args.imagebuildscript"
+    } 
+    
+       sh '''
+           bash $DOCKERSCRIPTPATH
+          '''
 }
